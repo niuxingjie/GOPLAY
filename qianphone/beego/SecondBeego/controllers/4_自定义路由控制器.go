d03efc9b20fd2d517612975212660a3fd811f782
jerrybox <jerryyoucan@foxmail.com>
2022-08-17 20:39:50 +0800

    自定义路由和控制器的使用

--- qianphone/beego/SecondBeego/controllers/4_自定义路由控制器.go
+++ qianphone/beego/SecondBeego/controllers/4_自定义路由控制器.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type CustomController struct {
+	beego.Controller
+}
+
+func (this *CustomController) GetUserId() {
+	id := this.Ctx.Input.Param(":id")
+	beego.Info("变量匹配ID:" + id)
+	this.Ctx.ResponseWriter.Write([]byte("ID:" + id))
+}
+
+func (this *CustomController) GetUserName() {
+	name := this.Ctx.Input.Param(":name")
+	beego.Info("变量【0-9】+匹配name:" + name)
+	this.Ctx.ResponseWriter.Write([]byte("name:" + name))
+}
--- qianphone/beego/SecondBeego/routers/3_正则路由.go
+++ qianphone/beego/SecondBeego/routers/3_正则路由.go
@@ -1,17 +1,17 @@
 package routers
 
-import (
-	"SecondBeego/controllers"
+// import (
+// 	"SecondBeego/controllers"
 
-	"github.com/astaxie/beego"
-)
+// 	"github.com/astaxie/beego"
+// )
 
-func init() {
-	beego.Router("/getUserId/:id", &controllers.RgController{})
+// func init() {
+// 	beego.Router("/getUserId/:id", &controllers.RgController{})
 
-	beego.Router("/getUserName/:name([0-9]+)", &controllers.RgController{})
+// 	beego.Router("/getUserName/:name([0-9]+)", &controllers.RgController{})
 
-	beego.Router("/getUserAge/:age:int", &controllers.RgController{})
+// 	beego.Router("/getUserAge/:age:int", &controllers.RgController{})
 
-	beego.Router("/getUserInfo/:info:string", &controllers.RgController{})
-}
+// 	beego.Router("/getUserInfo/:info:string", &controllers.RgController{})
+// }
