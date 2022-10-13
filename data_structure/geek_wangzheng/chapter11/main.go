21102954401a74853becad9a702f5f4986f1e06f
niuxingjie <niuxingjie@8lab.cn>
2022-10-13 18:38:24 +0800

    增加数据结构与算法课程目录

---
+++ data_structure/geek_wangzheng/chapter11/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main () {
+	// 冒泡排序
+
+	// 初始数组序列
+	var a [5]int = [5]int{1, 2, 3, 4} 
+	var n int = 5
+
+	// 打印初始数据
+
+	// 排序数据
+
+	// 打印新数组
+}
+
+
+func print_array(a []int){
+	for 
+}
+
+
+func bubble_sort_1(a []int, n int) []int {
+	// go 数组能直接赋值给切片参数吗？
+
+}
+
+
